70516aed109845e5edeba3d294b57ab22dbf3438
Dan Prowse <danprowse@hotmail.co.uk>
2021-12-22 17:28:47 +0000

    Addition of base theme files

--- index.php
+++ index.php
@@ -2,14 +2,17 @@
 
 get_header(); ?>
 
-<?php if (have_posts()) {
-  while(have_posts()) {
-    the_post(); ?>
-    <div>
-      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-      <?php the_content(); ?>
-    </div>
-  <?php }
+<?php
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+		?>
+		<div>
+			<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			<?php the_content(); ?>
+		</div>
+		<?php
+	}
 }
 
 get_footer();
