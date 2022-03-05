f06c5f8c3154d81e31334d4add59e9440fe4339e
Dan Prowse <danprowse@hotmail.co.uk>
2022-03-05 17:37:15 +0000

    foundation-sites added + improved folder structure

--- functions.php
+++ functions.php
@@ -24,14 +24,22 @@ add_action( 'wp_enqueue_scripts', 'enqueue_base_theme_scripts' );
 
 function enqueue_third_party_styles() {
 	// slick
-	wp_enqueue_style( 'slick-css', get_stylesheet_directory_uri() . '/src/third-party/css/slick.css', false, 'all' );
-	wp_enqueue_style( 'slick-theme-css', get_stylesheet_directory_uri() . '/src/third-party/css/slick-theme.css', 'slick-css', false, 'all' );
+	wp_enqueue_style( 'slick-css', get_stylesheet_directory_uri() . '/src/third-party/css/slick/slick.css', false, 'all' );
+	wp_enqueue_style( 'slick-theme-css', get_stylesheet_directory_uri() . '/src/third-party/css/slick/slick-theme.css', 'slick-css', false, 'all' );
+	// foundation-sites
+	wp_enqueue_style( 'fs-css', get_stylesheet_directory_uri() . '/src/third-party/css/foundation/foundation.css', false, 'all' );
+	wp_enqueue_style( 'fs-app-css', get_stylesheet_directory_uri() . '/src/third-party/css/foundation/app.css', false, 'all' );
+
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_third_party_styles' );
 
 function enqueue_third_party_scripts() {
 	// slick
-	wp_enqueue_script( 'slick-js', get_stylesheet_directory_uri() . '/src/third-party/js/slick.js', 'jquery', false, 'all' );
+	wp_enqueue_script( 'slick-js', get_stylesheet_directory_uri() . '/src/third-party/js/slick/slick.js', 'jquery', false, 'all' );
+	// foundation-sites
+	wp_enqueue_script( 'fs-what-input', get_stylesheet_directory_uri() . '/src/third-party/js/foundation/vendor/what-input.js', 'jquery', false, 'all' );
+	wp_enqueue_script( 'fs-foundation', get_stylesheet_directory_uri() . '/src/third-party/js/foundation/vendor/foundation.js', 'fs-what-input', false, 'all' );
+	wp_enqueue_script( 'fs-app', get_stylesheet_directory_uri() . '/src/third-party/js/foundation/app.js', 'fs-foundation', false, 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_third_party_scripts' );
 
